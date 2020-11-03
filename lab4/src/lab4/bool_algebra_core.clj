bd5a3a4e0967d3ddbfd0560a10a9fafc65e89878
Denis Gaskov <d.gaskov@g.nsu.ru>
2020-11-03 23:27:52 +0700

    Lab4: Add optional args to 'apply-translation-table' func, add documentation for it

--- lab4/src/lab4/bool_algebra_core.clj
+++ lab4/src/lab4/bool_algebra_core.clj
@@ -71,57 +71,103 @@
 
 ;; COMMON UTILS
 
+(defn- boolean-xor [a b] (if a (if b false true) (if b true false)))
+
 (defn args [expr]
   (rest expr))
 
-(defn apply-transform
-  [expr table]
+(defn apply-translation-table
+  "Appliex given transformation table to the expression.
+   Transformation table is a list of pairs (in fact, nested lists).
+   Nested list must contain a `rule` - pair of two values.
+   First is a `predicate`, which defines should we apply
+   this rule to the expression, or not (must return bool)
+   Second is a `transform` - function which takes an expression
+   with optional arguments, and returns new modified expression"
+  [expr translation-table & args]
   (if-let [transform (some (fn [rule]
                              (if ((first rule) expr)
                                (second rule)
                                false))
-                           table)]
-    (transform expr)
+                           translation-table)]
+    (transform expr args)
     (throw (ex-info
             "Could not found any rules for given expression"
             {:expresion expr
-             :number-of-rules (count table)}))))
+             :number-of-rules (count translation-table)}))))
 
 
+;; BOOLEAN ENGINE HELPER UTILS. NORMALLY YOU DO NOT NEED TO USE THEM
 
-;; STAGE 1. EXPRESSING EXPRESSIONS USING BASIS
-(declare express-in-basis)
+;; Stage 1. Expressing expressions in basis
 
-(def ^:private basis-table
+(declare ^:private express-in-basis)
+
+(def ^:private express-in-basis-table
+  "No arguments are used."
   (list
   ;;  Expressions, which are `atoms` - constants, variables
   ;;  should be passed as is.
    [(fn [expr] (or (constant? expr)
                    (variable? expr)))
-    identity]
+    (fn [expr _] expr)]
 
    [(fn [expr] (conjunction? expr))
-    (fn [expr] (let [[a b] (args expr)]
+    (fn [expr _] (let [[a b] (args expr)]
                  (conjunction (express-in-basis a)
                               (express-in-basis b))))]
 
    [(fn [expr] (disjunction? expr))
-    (fn [expr] (let [[a b] (args expr)]
+    (fn [expr _] (let [[a b] (args expr)]
                  (disjunction (express-in-basis a)
                               (express-in-basis b))))]
 
    [(fn [expr] (negation? expr))
-    (fn [expr] (let [[arg] (args expr)]
+    (fn [expr _] (let [[arg] (args expr)]
                  (negation (express-in-basis arg))))]
 
    [(fn [expr] (implication? expr))
-    (fn [expr] (let [[a b] (args expr)]
+    (fn [expr _] (let [[a b] (args expr)]
                  (disjunction (negation a) b)))]))
 
-(defn express-in-basis [expr] (apply-transform expr basis-table))
+(defn express-in-basis
+  [expr]
+  (apply-translation-table expr express-in-basis-table))
 
 
+;; Stage 2. Push negation to constants and variables
+(declare push-negation-to-atoms-with-arg)
 
-;; STAGE 2. MOVE NEGATION INTO CONSTANTS AND VARIABLES
+(def ^:private push-negation-to-atoms-table
+  "Uses single argument - `use-negation`, which stores current negation-carry state"
+  (list
+  ;;  Expressions, which are `atoms` - constants, variables
+  ;;  should be marked with pushed negation, if needed.
+   [(fn [expr] (or (constant? expr)
+                   (variable? expr)))
+    (fn [expr use-negation]
+      (if use-negation
+        (negation expr)
+        expr))]
 
-;; (def ^:private negation-table)
+   [(fn [expr] (conjunction? expr))
+    (fn [expr use-negation] (let [[a b] (args expr)]
+                              (conjunction (push-negation-to-atoms-with-arg a use-negation)
+                                           (push-negation-to-atoms-with-arg b use-negation))))]
+
+   [(fn [expr] (disjunction? expr))
+    (fn [expr use-negation] (let [[a b] (args expr)]
+                              (disjunction (push-negation-to-atoms-with-arg a use-negation)
+                                           (push-negation-to-atoms-with-arg b use-negation))))]
+   
+   [(fn [expr] (negation? expr))
+    (fn [expr use-negation] (let [[arg] (args expr)]
+                              (negation (push-negation-to-atoms-with-arg arg (boolean-xor use-negation true)))))]))
+
+(defn- push-negation-to-atoms-with-arg
+  [expr use-negation]
+  (apply-translation-table expr push-negation-to-atoms-table use-negation))
+
+(defn push-negation-to-atoms
+  [expr]
+  (push-negation-to-atoms-with-arg expr false))
